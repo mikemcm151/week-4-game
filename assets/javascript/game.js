b51184cb8681fa90428f71fb38da385eed39ea04
mikemcm151 <mikemcm151@gmail.comgit config --global user.name mikemcm151git config --global user.email mikemcm151@gmail.com>
2017-11-15 17:47:15 -0600

    Latest and Greatest HW4

---
+++ assets/javascript/game.js
@@ -0,0 +1,69 @@
+// $(document).ready(function() {
+
+// }
+
+
+  $("#firstgem").on("click", function() {
+    var randomNumber = "";
+    for (var i=0; i<12; i++){
+      var random = Math.floor(Math.random() + 1);
+    }
+  })
+    $("#secondgem").on("click", function(){
+      alert("test");
+    })
+      $("#thirdgem").on("click", function(){
+        alert("test");
+      })
+        $("#fourthgem").on("click", function(){
+          alert("test");
+        })
+
+
+// // all 4 gems
+
+// $("#firstgem").on("click", function() {
+// 	var randomNumber = "";
+// 	for (var i = 0; i < 12; i++) {
+// 		var random = Math.floor(Math.random() * 12) + 1;
+// 	}}
+
+// $("#secondgem").on("click", function() {
+// 	var randomNumber = "";
+// 	for (var i = 0; i < 12; i++) {
+// 		var random = Math.floor(Math.random() * 12) + 1;
+// 	}}
+
+// $("#thirdgem").on("click", function() {
+// 	var randomNumber = "";
+// 	for (var i = 0; i < 12; i++) {
+// 		var random = Math.floor(Math.random() * 12) + 1;
+// 	}}
+
+// $("#fourthgem").on("click", function() {
+// 	var randomNumber = "";
+// 	for (var i = 0; i < 12; i++) {
+// 		var random = Math.floor(Math.random() * 12) + 1;
+// 	}}
+
+
+// random number generator
+// var randomnumber = 0
+// $("random").on("click", function() {
+// 	var randomNumber = "";
+// 	for (var i = 19; i < 122; i++) {
+// 		var random = Math.floor((Math.random() * 122) + 1;
+// 	}}
+
+// $("random") = 0
+// 	for (var i = 0; i < 12; i++) {
+// 		var random = Math.floor(Math.random() * 12) + 1;
+// function random() {
+// 	return Math.floor((Math.random() * 122) + 1;
+}
+
+// }
+
+// must reset the values!!!
+// get a random number function period. just to call at first. then apply to each 
+// make it work first, make it pretty, then make it fast
